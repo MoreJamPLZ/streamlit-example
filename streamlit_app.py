766e6969c7aace47b085a8ce8e7d2e78638483a1
MoreJamPLZ <121439406+MoreJamPLZ@users.noreply.github.com>
2023-12-19 13:20:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,17 +47,32 @@ In the meantime, below is an example of what you can do with just a few lines of
 #         # print(uploaded_file)
 #         st.write("You selected the file:", uploaded_file.name)
 
-if 'clicked' not in st.session_state:
-    st.session_state.clicked = False
+# Once we have the dependencies, add a selector for the app mode on the sidebar.
+st.sidebar.title("What to do")
+app_mode = st.sidebar.selectbox("Choose the app mode",
+    ["Show instructions", "Upload file", "Show the source code"])
+if app_mode == "Show instructions":
+    st.sidebar.success('To continue select "Upload file".')
+elif app_mode == "Show the source code":
+    readme_text.empty()
+    st.code(get_file_content_as_string("streamlit_app.py"))
+elif app_mode == "Run the app":
+    readme_text.empty()
+    run_the_app()
 
-def set_clicked():
-    st.session_state.clicked = True
-
-st.button('Upload File', on_click=set_clicked)
-if st.session_state.clicked:
-    uploaded_file = st.file_uploader("Choose a file")
-    print(uploaded_file)
-    if uploaded_file is not None:
-        # print(uploaded_file)
-        st.write("You selected the file:", uploaded_file.name)
+# This is the main app app itself, which appears when the user selects "Run the app".
+def run_the_app():
+    if 'clicked' not in st.session_state:
+        st.session_state.clicked = False
+    
+    def set_clicked():
+        st.session_state.clicked = True
+    
+    st.button('Upload File', on_click=set_clicked)
+    if st.session_state.clicked:
+        uploaded_file = st.file_uploader("Choose a file")
+        print(uploaded_file)
+        if uploaded_file is not None:
+            # print(uploaded_file)
+            st.write("You selected the file:", uploaded_file.name)
         
